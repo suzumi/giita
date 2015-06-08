d63cc26e4ca6a6604bfc9ece23e7673a5190db4e
k-shibue <shibue@blue-corporation.jp>
2015-06-08 14:25:08 +0900

    実行処理判定の修正

--- app/Console/Commands/Tag.php
+++ app/Console/Commands/Tag.php
@@ -15,36 +15,36 @@ class Tag
     }
 
     /**
-     * 日曜日になると次週の週報タグを生成する
+     * 月曜日になると今週の週報タグを生成する
      */
     public function buildNextWeekTag()
     {
-        \Log::info('Start generate tag of Next week. ');
+        \Log::info('Start generate tag of This week. ');
         $dt = new Carbon();
 //        $dt->setTestNow($dt->createFromDate(2015, 5, 31));
-        if($dt->now()->dayOfWeek === Carbon::SUNDAY) {
+        if($dt->now()->dayOfWeek === Carbon::MONDAY) {
             //翌月
             $nextMonth =  $dt->parse('next month')->month;
             // 現在が何週目か
-            echo '今日は'.$dt->now()->month.'月の第'.$dt->now()->weekOfMonth.'週目です'. PHP_EOL;
+            \Log::info('今日は'.$dt->now()->month.'月の第'.$dt->now()->weekOfMonth.'週目です');
             //今週の金曜を取得
             $thisFriday = $dt->parse('this friday');
-            echo '来週の金曜は' . $thisFriday.'<br>';
+            \Log::info('来週の金曜は' . $thisFriday);
             // 月またぎの場合
             if($thisFriday->month === $nextMonth) {
-                echo $thisFriday->year . '年の'. $thisFriday->month . '月の第'.$thisFriday->weekOfMonth.'週目です'. PHP_EOL;
+                \Log::info($thisFriday->year . '年の'. $thisFriday->month . '月の第'.$thisFriday->weekOfMonth.'週目です');
                 $nextWeekTag = '週報@'. $thisFriday->year. '年'. $thisFriday->month. '月第'. $thisFriday->weekOfMonth. '週';
                 $this->tag->tag = $nextWeekTag;
                 $this->tag->save();
             } else {
-                echo '翌週は'.$thisFriday->month.'月の第'.$thisFriday->weekOfMonth.'週目です'. PHP_EOL;
+                \Log::info('翌週は'.$thisFriday->month.'月の第'.$thisFriday->weekOfMonth.'週目です');
                 $nextWeekTag = '週報@'. $thisFriday->year. '年'. $thisFriday->month. '月第'. $thisFriday->weekOfMonth. '週';
                 $this->tag->tag = $nextWeekTag;
                 $this->tag->save();
             }
             \Log::info('End generate tag');
         } else {
-            \Log::info('Today is not Sunday.');
+            \Log::info('Today is not Monday.');
         }
     }
 }
